fc0a43b078290d94c85ff4feb9e2370ff42f61a8
DanRHenry <danielrhenry@gmail.com>
2025-07-22 04:45:14 -0400

    added more build scripts and event listeners

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-import {buildAboutContent} from "./components/buildAboutContent.js"
-
-import { buildContactContent } from "./components/buildContactContent.js"
-
 import { buildSplashpageContent } from "./components/buildSplashpageContent.js"
+import {buildAboutContent} from "./components/buildAboutContent.js"
+import {buildScheduleAppointment} from "./components/buildScheduleAppointment.js"
+import { buildmedicineContent } from "./components/buildMedicineContent.js"
+import { buildAddressContent } from "./components/buildAddressContent.js"
 
-// buildAboutContent()
-buildContactContent()
+const homeBtn = document.getElementById("homeBtn")
+const aboutBtn = document.getElementById("aboutBtn")
+const scheduleApptBtn = document.getElementById("scheduleApptBtn")
+const medicineBtn = document.getElementById("medicineBtn")
+const addressInfoBtn = document.getElementById("addressInfo")
+
+homeBtn.addEventListener("click", buildSplashpageContent)
+aboutBtn.addEventListener("click", buildAboutContent)
+scheduleApptBtn.addEventListener("click", buildScheduleAppointment)
+medicineBtn.addEventListener("click", buildmedicineContent)
+addressInfoBtn.addEventListener("click", buildAddressContent)
+
 // buildSplashpageContent()
+// buildAboutContent()
+// buildScheduleAppointment()
+buildAddressContent()
+// buildmedicineContent()
