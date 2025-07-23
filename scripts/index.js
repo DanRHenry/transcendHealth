42db5d18f1300cb91b40a260c576ae387697e456
DanRHenry <danielrhenry@gmail.com>
2025-07-23 15:41:47 -0400

    added map functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,90 @@
-import { buildSplashpageContent } from "./components/buildSplashpageContent.js"
-import {buildAboutContent} from "./components/buildAboutContent.js"
-import {buildScheduleAppointment} from "./components/buildScheduleAppointment.js"
-import { buildmedicineContent } from "./components/buildMedicineContent.js"
-import { buildAddressContent } from "./components/buildAddressContent.js"
+import { buildLandingpageContent } from "./components/buildLandingpageContent.js";
+import { buildAboutContent } from "./components/buildAboutContent.js";
+import { buildScheduleAppointment } from "./components/buildScheduleAppointment.js";
+import { buildMedicineContent } from "./components/buildMedicineContent.js";
+import { buildAddressContent } from "./components/buildAddressContent.js";
+import { buildmap } from "./buildmap.js";
 
-const homeBtn = document.getElementById("homeBtn")
-const aboutBtn = document.getElementById("aboutBtn")
-const scheduleApptBtn = document.getElementById("scheduleApptBtn")
-const medicineBtn = document.getElementById("medicineBtn")
-const addressInfoBtn = document.getElementById("addressInfo")
+const homeBtn = document.getElementById("homeBtn");
+const aboutBtn = document.getElementById("aboutBtn");
+const scheduleApptBtn = document.getElementById("scheduleApptBtn");
+const medicineBtn = document.getElementById("medicineBtn");
+const addressInfoBtn = document.getElementById("addressInfo");
 
-homeBtn.addEventListener("click", buildSplashpageContent)
-aboutBtn.addEventListener("click", buildAboutContent)
-scheduleApptBtn.addEventListener("click", buildScheduleAppointment)
-medicineBtn.addEventListener("click", buildmedicineContent)
-addressInfoBtn.addEventListener("click", buildAddressContent)
+homeBtn.addEventListener("click", handleHomeBtnClick);
+aboutBtn.addEventListener("click", handleAboutBtnClick);
+scheduleApptBtn.addEventListener("click", handleScheduleApptBtnClick);
+medicineBtn.addEventListener("click", handleMedicineBtnClick);
+addressInfoBtn.addEventListener("click", handleAddressBtnClick);
 
-buildSplashpageContent()
-// buildAboutContent()
-// buildScheduleAppointment()
-// buildAddressContent()
-// buildmedicineContent()
+
+function handleHomeBtnClick(e) {
+        hidemap()
+
+  e.preventDefault();
+  clearBtnStyling();
+  homeBtn.style.fontWeight = "bold";
+  homeBtn.style.borderColor = "white";
+  buildLandingpageContent();
+}
+
+function handleAboutBtnClick(e) {
+  e.preventDefault();
+  clearBtnStyling();
+  hidemap()
+  aboutBtn.style.fontWeight = "bold";
+  aboutBtn.style.borderColor = "white";
+  buildAboutContent();
+}
+
+function handleScheduleApptBtnClick(e) {
+  e.preventDefault();
+  clearBtnStyling();
+  hidemap()
+  scheduleApptBtn.style.fontWeight = "bold";
+  scheduleApptBtn.style.borderColor = "white";
+  buildScheduleAppointment();
+}
+
+function handleAddressBtnClick(e) {
+  e.preventDefault();
+  clearBtnStyling();
+  addressInfoBtn.style.fontWeight = "bold";
+  addressInfoBtn.style.borderColor = "white";
+  buildAddressContent(addressContent);
+}
+
+function handleMedicineBtnClick(e) {
+  e.preventDefault();
+  clearBtnStyling();
+  hidemap()
+  medicineBtn.style.fontWeight = "bold";
+  medicineBtn.style.borderColor = "white";
+  buildMedicineContent();
+}
+
+function clearBtnStyling() {
+  const buttons = document.getElementsByClassName("button");
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style = null;
+    // console.log(buttons[i])
+  }
+}
+
+function hidemap () {
+    const map = document.getElementById('hiddenMap')
+    if (map) {
+    map.style.visibility = "hidden"
+    map.style.height = "0"
+    map.style.flex = null
+    }
+}
+
+    window.addEventListener("load", () => {
+        buildmap()
+    })
+
+homeBtn.click();
+
+
